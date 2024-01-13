67f91fdda4f4fdd70896718fe1df32bc42d4d6ac
Skilledgamer101 <mansoorlunawadi@yahoo.ca>
2024-01-13 20:20:03 +0000

    Fix optimized

--- analyze_text.py
+++ analyze_text.py
@@ -65,3 +65,52 @@ def async_detect_document(gcs_source_uri, gcs_destination_uri):
 # and pass in output string name into write_to_text
 
 
+def write_to_text(gcs_destination_uri):
+    # Once the request has completed and the output has been
+    # written to GCS, we can list all the output files.
+    storage_client = storage.Client()
+
+    match = re.match(r'gs://([^/]+)/(.+)', gcs_destination_uri)
+    bucket_name = match.group(1)
+    prefix = match.group(2)
+
+    bucket = storage_client.get_bucket(bucket_name)
+
+    # List objects with the given prefix.
+    blob_list = list(bucket.list_blobs(prefix=prefix))
+    print('Output files:')
+
+    transcription = open(transcription.txt, w)
+
+    for blob in blob_list:
+        print(blob.name)
+
+    # Process the first output file from GCS.
+    # Since we specified batch_size=2, the first response contains
+    # the first two pages of the input file.
+    for n in  range(len(blob_list)):
+        output = blob_list[n]
+
+        json_string = output.download_as_string()
+        response = json.loads(json_string)
+
+
+        # The actual response for the first page of the input file.
+        for m in range(len(response['responses'])):
+
+            first_page_response = response['responses'][m]
+
+            try:
+                annotation = first_page_response['fullTextAnnotation']
+            except(KeyError):
+                print(No annotation for this page.)
+
+            # Here we print the full text from the first page.
+            # The response contains more information:
+            # annotation/pages/blocks/paragraphs/words/symbols
+            # including confidence scores and bounding boxes
+            print('Full text:\n')
+            print(annotation['text'])
+            
+            with open(transcription.txt, a+, encoding=utf-8) as f:
+                f.write(annotation['text'])
