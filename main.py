09231448d5624458c26a152ae3a20b085201297a
Mansoor Lunawadi <136755975+Skilledgamer101@users.noreply.github.com>
2024-01-15 20:52:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
 import analyze_text as at
-import form
-import form_filler
 
-# src = input("Enter Google Cloud URL of PDF:\n")
-# dest = input("Enter Google Cloud URL of destination:\n")
+
+src = input("Enter Google Cloud URL of PDF:\n")
+dest = input("Enter Google Cloud URL of destination:\n")
 
 # get text from PDF (save it to a file in curr dir)
-# bucket, prefix = at.async_detect_document(src, dest)
-# at.write_to_text(bucket, prefix)
-link = input("Please enter link of google form:\n")
-questions = form_filler.get(link)
-# get answers to write based on PDF content (in folder)
-ans = form.write_post(questions)
-# fill out form and print new form link
-form_filler.fill(link, ans)
+bucket, prefix = at.async_detect_document(src, dest)
+at.write_to_text(bucket, prefix)
 
 
 
